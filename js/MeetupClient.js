f4a92b3e46f11090706097c8b83a4bc0c73d09b8
Adam Bergen <bergen@gmail.com>
2014-11-05 20:10:09 -0600

    now works by taking in user input for zip code and area of interest

--- js/MeetupClient.js
+++ js/MeetupClient.js
@@ -20,7 +20,8 @@
 			this.render();
 		},
 		
-		template: "<h3>{name}</h3><hr><h6>{description}</h6>",
+		//template: "<h3>{name}</h3><hr><h6>{description}</h6>",
+		template: "<h3>{name}</h3><hr><p>{description}</p>",
 		render: function(){
 			this.el.innerHTML = _.template(this.template, this.options);
 		}
@@ -31,20 +32,30 @@ function MeetupClient(options){
 	
 	this.options = {apikey: "284b6a257c7c73785101a423976502c"};
 
-	this.init();
+	this.Routing();
 }
 
-MeetupClient.prototype.queryAPI = function(zipcode, search_term){
+MeetupClient.prototype.createInputObject = function(){
 
-//	var input = this.createInputObject();
+	var input = {};
+	$(':input').each(function(){
+		input[this.name] = this.value;
+	})
+	console.log(input);
+	return input;
+}
+
+MeetupClient.prototype.queryAPI = function(){
+
+	var input = this.createInputObject();
 
 	var url = [ "http://api.meetup.com/2/groups.json/",
 				"?zip=",
-				zipcode,
-				//input.zip,
+				//zipcode,
+				input.zipcode,
 				"&topic=",
-				search_term,
-				//input.topic,
+				//search_term,
+				input.group,
 				"&order=members&key=",
 				this.options.apikey,
 				"&callback=?" 
@@ -60,7 +71,7 @@ MeetupClient.prototype.queryAPI = function(zipcode, search_term){
 
 MeetupClient.prototype.makeRequest = function(){
 
-	$.when(this.queryAPI("77002", "javascript")
+	$.when(this.queryAPI()
 		).then(function(data){
 			
 			console.log(data);
@@ -72,12 +83,26 @@ MeetupClient.prototype.makeRequest = function(){
 		});
 };
 
-MeetupClient.prototype.init = function(){
+// MeetupClient.prototype.init = function(){
+
+// 	var self = this;
+
+// 		self.makeRequest();
+
+
+// };
+
+MeetupClient.prototype.Routing = function(){
 
 	var self = this;
 
+	Path.map("#/results").to(function(){
+		
 		self.makeRequest();
+	})
 
+	Path.root("#/");
+	Path.listen();
 
 };
 
@@ -100,23 +125,138 @@ window.MeetupClient = MeetupClient;
 
 
 
-// MeetupClient.prototype.Routing = function(){
+
+
+// ORIGINAL WORKING CODE BEFORE ASKING FOR USER INPUT
+// 
+// (function(window, undefined){
+// 	"use strict";
+
+// 	var meetupView = Backbone.View.extend({
+
+// 		tagname: "div",
+// 		className: "meetups",
+// 		initialize: function(opts){
+
+// 			this.options = _.extend(
+// 				{},
+// 				{
+// 					$container: $('body')
+// 				},
+// 				opts
+// 				);
+
+// 			this.options.$container.append(this.el);
+
+// 			this.render();
+// 		},
+		
+// 		//template: "<h3>{name}</h3><hr><h6>{description}</h6>",
+// 		template: "<h3>{name}</h3><hr><p>{description}</p>",
+// 		render: function(){
+// 			this.el.innerHTML = _.template(this.template, this.options);
+// 		}
+// 	})
+
+
+// function MeetupClient(options){
+	
+// 	this.options = {apikey: "284b6a257c7c73785101a423976502c"};
+
+// 	this.init();
+// }
+
+// // MeetupClient.prototype.createInputObject = function(){
+
+// // 	var input = {};
+// // 	$(':input').each(function(){
+// // 		input[this.name] = this.value;
+// // 	})
+// // 	console.log(input);
+// // 	return input;
+// // }
+
+// MeetupClient.prototype.queryAPI = function(zipcode, search_term){
+
+// 	var input = this.createInputObject();
+
+// 	var url = [ "http://api.meetup.com/2/groups.json/",
+// 				"?zip=",
+// 				zipcode,
+// 				//input.zipcode,
+// 				"&topic=",
+// 				search_term,
+// 				//input.group,
+// 				"&order=members&key=",
+// 				this.options.apikey,
+// 				"&callback=?" 
+
+// 				];
+
+// 	return $.getJSON(url.join('')).then(function(data){
+// 		//console.log(data);
+// 		return data;
+// 	});
+
+// };
+
+// MeetupClient.prototype.makeRequest = function(){
+
+// 	$.when(this.queryAPI("77002", "javascript")
+// 		).then(function(data){
+			
+// 			console.log(data);
+
+// 			arguments[0].results.forEach(function(data){
+// 				new meetupView(data);
+// 			})
+
+// 		});
+// };
+
+// MeetupClient.prototype.init = function(){
 
 // 	var self = this;
 
-// 	Path.map("#/results").to(function(){
-// 		$.when()
-// 	})
+// 		self.makeRequest();
+
+
 // };
 
 
+// window.MeetupClient = MeetupClient;
 
-// MeetupClient.prototype.createInputObject = function(){
+// }) (window, undefined);
 
-// 	var input = {};
-// 	$(':input').each(function(){
-// 		input[this.name] = this.value;
-// 	})
-// 	console.log(input);
-// 	return input;
-// }
+// // MeetupClient.prototype.getGeo = function(){
+	
+// // 	var promise = $.Deferred();
+	
+// // 	navigator.geolocation.getCurrentPosition(function(){
+// // 		promise.resolve(arguments[0]);
+// // 	});
+
+// // 	console.log(promise);
+// // 	return promise;
+// // };
+
+
+
+// // MeetupClient.prototype.Routing = function(){
+
+// // 	var self = this;
+
+// // 	Path.map("#/results").to(function(){
+// // 		$.when()
+// // 	})
+// // };
+
+
+
+
+
+
+
+
+
+
